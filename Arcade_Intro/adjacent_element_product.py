be14cc71e5149998e8ec75a1af7d1e05c55c199f
Christopher Okoro <christopherokoro007@gmail.com>
2022-04-26 12:06:14 +0100

    added more solutions

---
+++ Arcade_Intro/adjacent_element_product.py
@@ -0,0 +1,36 @@
+"""
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+solution(inputArray) = 21.
+
+7 and 3 produce the largest product.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer inputArray
+
+An array of integers containing at least two elements.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 10,
+-1000 ≤ inputArray[i] ≤ 1000.
+
+[output] integer
+
+The largest product of adjacent elements.
+
+
+"""
+
+from typing import Union
+
+
+def solution(inputArray: Union[tuple, list]) -> int:
+    # create a generator using a zipper
+    paired_list = (pair[0] * pair[1] for pair in zip(inputArray[:-1], inputArray[1:]))
+    return max(paired_list)
---
+++ Arcade_Intro/shape_area.py
@@ -0,0 +1,34 @@
+"""
+Below we will define an n-interesting polygon.
+Your task is to find the area of a polygon for a given n.
+
+A 1-interesting polygon is just a square with a side of length 1.
+An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side.
+
+Example
+
+For n = 2, the output should be
+solution(n) = 5;
+For n = 3, the output should be
+solution(n) = 13.
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] integer n
+
+Guaranteed constraints:
+1 ≤ n < 104.
+
+[output] integer
+
+The area of the n-interesting polygon.
+"""
+
+
+def solution(polygon_number: int) -> int:
+    term_a = 2 * (polygon_number ** 2)
+    term_b = 2 * polygon_number
+    term_c = term_a - term_b
+    result = term_c + 1
+    return result
