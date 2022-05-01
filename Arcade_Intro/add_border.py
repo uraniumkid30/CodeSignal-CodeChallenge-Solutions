2fcb36803e4b110bb2483adb02e23b012f8abe54
uraniumkid30 <uraniumkid30@gmail.com>
2022-05-02 00:07:18 +0100

    added solution for add bother solution

---
+++ Arcade_Intro/add_border.py
@@ -0,0 +1,37 @@
+"""
+Given a rectangular matrix of characters, add a border of asterisks(*) to it.
+
+Example
+
+For
+
+picture = ["abc",
+           "ded"]
+the output should be
+
+solution(picture) = ["*****",
+                      "*abc*",
+                      "*ded*",
+                      "*****"]
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.string picture
+
+A non-empty array of non-empty equal-length strings.
+
+Guaranteed constraints:
+1 ≤ picture.length ≤ 100,
+1 ≤ picture[i].length ≤ 100.
+
+[output] array.string
+
+The same matrix of characters, framed with a border of asterisks of width 1.
+"""
+
+
+def solution(picture: list) -> list:
+    max_len: int = max(map(len, picture))
+    additive: list = ["**" + ("*" * max_len)]
+    return additive + [f"*{x}*" for x in picture] + additive
