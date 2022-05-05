775e33c72401279bc5a9c9880f5eefb5b4074f2b
uraniumkid30 <uraniumkid30@gmail.com>
2022-05-05 03:49:28 +0100

    added solution for avoid obstacles problem.

---
+++ Arcade_Intro/avoid_obstacles.py
@@ -0,0 +1,44 @@
+"""
+You are given an array of integers representing coordinates of obstacles situated on a straight line.
+
+Assume that you are jumping from the point with coordinate 0 to the right. You are allowed only to make jumps of the same length represented by some integer.
+
+Find the minimal length of the jump enough to avoid all the obstacles.
+
+Example
+
+For inputArray = [5, 3, 6, 7, 9], the output should be
+solution(inputArray) = 4.
+
+Check out the image below for better understanding:
+
+
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer inputArray
+
+Non-empty array of positive integers.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 1000,
+1 ≤ inputArray[i] ≤ 1000.
+
+[output] integer
+
+The desired length.
+"""
+
+
+def solution(inputArray: list):
+    position: int = 0
+    step: int = 1
+    while position <= max(inputArray):
+        if position in inputArray:
+            step += 1
+            position = 0
+        else:
+            position += step
+    return step
