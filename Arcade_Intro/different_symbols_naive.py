a38fad041a8e21153b72f30dafd83d329120576f
uraniumkid30 <uraniumkid30@gmail.com>
2022-05-12 09:09:16 +0100

    added solution to different symbols naive problem

---
+++ Arcade_Intro/different_symbols_naive.py
@@ -0,0 +1,28 @@
+"""
+Given a string, find the number of different characters in it.
+
+Example
+
+For s = "cabca", the output should be
+solution(s) = 3.
+
+There are 3 different characters a, b and c.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] string s
+
+A string of lowercase English letters.
+
+Guaranteed constraints:
+3 ≤ s.length ≤ 1000.
+
+[output] integer
+
+"""
+
+
+def solution(letters: str) -> int:
+    return len(list(set(letters)))
