b5e0acb54ae9f602f96fed1f11792894b7365e87
uraniumkid30 <uraniumkid30@gmail.com>
2022-05-14 23:43:08 +0100

    added solution to longest digit prefix problem

---
+++ Arcade_Intro/longest_digit_prefix.py
@@ -0,0 +1,27 @@
+"""
+Given a string, output its longest prefix which contains only digits.
+
+Example
+
+For inputString = "123aa1", the output should be
+solution(inputString) = "123".
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] string inputString
+
+Guaranteed constraints:
+3 ≤ inputString.length ≤ 100.
+
+[output] string
+"""
+
+import re
+
+
+def solution(inputString: str) -> str:
+    pattern: str = "\d+"
+    match_result = re.match(pattern, inputString)
+    return match_result.group() if match_result else ""
