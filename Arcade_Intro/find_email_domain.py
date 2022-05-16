253e740e3592877fb84e47bb174e2fbb82a3a09b
uraniumkid30 <uraniumkid30@gmail.com>
2022-05-16 21:27:11 +0100

    added solution to find email domain problem

---
+++ Arcade_Intro/find_email_domain.py
@@ -0,0 +1,28 @@
+"""
+An email address such as "John.Smith@example.com" is made up of a local part ("John.Smith"), an "@" symbol, then a domain part ("example.com").
+
+The domain name part of an email address may only consist of letters, digits, hyphens and dots. The local part, however, also allows a lot of different special characters. Here you can look at several examples of correct and incorrect email addresses.
+
+Given a valid email address, find its domain part.
+
+Example
+
+For address = "prettyandsimple@example.com", the output should be
+solution(address) = "example.com";
+For address = "fully-qualified-domain@codesignal.com", the output should be
+solution(address) = "codesignal.com".
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] string address
+
+Guaranteed constraints:
+10 ≤ address.length ≤ 50.
+
+[output] string
+"""
+
+
+def solution(address: str) -> str:
+    return address.split("@")[-1]
