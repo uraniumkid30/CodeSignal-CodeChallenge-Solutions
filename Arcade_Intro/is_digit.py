aca11305265baad4bb5c4d44f4efecedf1d21bd0
uraniumkid30 <uraniumkid30@gmail.com>
2022-05-19 23:10:30 +0100

    added solution to line encoding problem

---
+++ Arcade_Intro/is_digit.py
@@ -0,0 +1,28 @@
+"""
+Determine if the given character is a digit or not.
+
+Example
+
+For symbol = '0', the output should be
+solution(symbol) = true;
+For symbol = '-', the output should be
+solution(symbol) = false.
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] char symbol
+
+A character which is either a digit or not.
+
+Guaranteed constraints:
+Given symbol is from ASCII table.
+
+[output] boolean
+
+true if symbol is a digit, false otherwise.
+"""
+
+
+def solution(symbol):
+    return symbol.isdigit()
