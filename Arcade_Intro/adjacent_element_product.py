a73eb08058ab405fd8e4aeb365e596adce95676a
uraniumkid30 <uraniumkid30@gmail.com>
2022-05-21 20:45:53 +0100

    updated adjacent element product solution

--- Arcade_Intro/adjacent_element_product.py
+++ Arcade_Intro/adjacent_element_product.py
@@ -28,8 +28,10 @@ The largest product of adjacent elements.
 """
 
 from typing import Union
+from .utils import timefunc
 
 
+@timefunc("Adjacent Element Product")
 def solution(inputArray: Union[tuple, list]) -> int:
     # create a generator using a zipper
     paired_list = (pair[0] * pair[1] for pair in zip(inputArray[:-1], inputArray[1:]))
