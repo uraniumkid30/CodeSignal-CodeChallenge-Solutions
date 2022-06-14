5446832f351a6bcd5120334b37fff964d31cdb03
uraniumkid30 <uraniumkid30@gmail.com>
2022-06-14 23:23:33 +0100

    Added test for absolute values sum minimization.

---
+++ Arcade/Arcade_Intro/arcade_tests/test_absolute_values_sum_minimization.py
@@ -0,0 +1,38 @@
+from ..absolute_value_sum_minimization import solution
+import unittest
+
+
+class TestAbsoluteValuesSumMinimization(unittest.TestCase):
+    def test_case_1(self):
+        inputArray: list = [2, 4, 7]
+        expected_result: int = 4
+        result = solution(inputArray)
+        self.assertEqual(expected_result, result)
+
+    def test_case_2(self):
+        inputArray: list = [-1, -2]
+        expected_result: int = 2
+        result = solution(inputArray)
+        self.assertEqual(expected_result, result)
+
+    def test_case_3(self):
+        inputArray: list = [5, 1, 2, 3, 1, 4]
+        expected_result: int = 6
+        result = solution(inputArray)
+        self.assertEqual(expected_result, result)
+
+    def test_case_4(self):
+        inputArray: list = [-23, 4, -3, 8, -12]
+        expected_result: int = -12
+        result = solution(inputArray)
+        self.assertEqual(expected_result, result)
+
+    def test_case_5(self):
+        inputArray: list = [5, 6, -4, 2, 3, 2, -23]
+        expected_result: int = 30
+        result = solution(inputArray)
+        self.assertEqual(expected_result, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
