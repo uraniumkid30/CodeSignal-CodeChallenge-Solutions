5f33c93c6251d97ea0e9c424ab2ea6e1ed42e726
uraniumkid30 <uraniumkid30@gmail.com>
2022-06-21 22:25:22 +0100

    added test for extract esch kth solution

--- Arcade/Arcade_Intro/arcade_tests/test_extract_each_kth.py
+++ Arcade/Arcade_Intro/arcade_tests/test_extract_each_kth.py
@@ -3,4 +3,41 @@ import unittest
 
 
 class TestExtractEachKth(unittest.TestCase):
-    pass
+    def test_case_1(self):
+        inputArray: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        k_value: int = 3
+        expected_result: list = [1, 2, 4, 5, 7, 8, 10]
+        result = solution(inputArray,k_value)
+        self.assertEqual(expected_result, result)
+
+    def test_case_2(self):
+        inputArray: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        k_value: int = 3
+        expected_result: list = [1, 2, 4, 5, 7, 8, 10]
+        result = solution(inputArray,k_value)
+        self.assertEqual(expected_result, result)
+
+    def test_case_3(self):
+        inputArray: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        k_value: int = 3
+        expected_result: list = [1, 2, 4, 5, 7, 8, 10]
+        result = solution(inputArray,k_value)
+        self.assertEqual(expected_result, result)
+
+    def test_case_4(self):
+        inputArray: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        k_value: int = 3
+        expected_result: list = [1, 2, 4, 5, 7, 8, 10]
+        result = solution(inputArray,k_value)
+        self.assertEqual(expected_result, result)
+
+    def test_case_5(self):
+        inputArray: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        k_value: int = 3
+        expected_result: list = [1, 2, 4, 5, 7, 8, 10]
+        result = solution(inputArray,k_value)
+        self.assertEqual(expected_result, result)
+
+
+if __name__ == "__main__":
+    unittest.main()
